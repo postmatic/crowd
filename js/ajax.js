f38369354d8ca2f18ea69b666918564678fd98d8
Ronald Huereca <ronalfy@gmail.com>
2015-09-21 01:01:34 -0500

    Fixing JS errors on reporting errors.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- js/ajax.js
+++ js/ajax.js
@@ -10,7 +10,15 @@ function safe_report_comments_flag_comment( comment_id, nonce, result_id ) {
 				withCredentials: true
 			}
 		},
-		function(data) { jQuery( '#'+result_id).html(data); }
+		function(data) { 
+    		$parent = jQuery( '#comment-' + comment_id );
+    		$child = $parent.find( '#' + result_id ).html( data );
+    		setTimeout( function() {
+        		$child.fadeOut( 'slow', function() {
+            		//todo maybe do some ajax and see if flagged here for Epoch
+                } );
+            }, 2000 );
+        }
 	);
 	return false;
 }
