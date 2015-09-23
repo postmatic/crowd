c55ad607823a0db023490d2a3356b0774b624e8b
Ronald Huereca <ronalfy@gmail.com>
2015-09-22 20:13:22 -0500

    Version number on script.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- crowd-control.php
+++ crowd-control.php
@@ -110,7 +110,7 @@ if ( !class_exists( "Crowd_Control" ) ) {
     		*/
 			$ajaxurl = apply_filters( 'pmcc_report_comments_ajax_url', $ajaxurl );
 
-			wp_enqueue_script( $this->_plugin_prefix . '-ajax-request', $this->plugin_url . '/js/ajax.js', array( 'jquery' ) );
+			wp_enqueue_script( $this->_plugin_prefix . '-ajax-request', $this->plugin_url . '/js/ajax.js', array( 'jquery' ), '20150922', true );
 			
 			/* Localize ajaxurl and error messages */
 			$localize_script_vars = array(
